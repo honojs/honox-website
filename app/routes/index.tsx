d3522c7f36483e8ba6815b9234a522be168071ac
NEKOYASAN <hello@nekoyasan.me>
2025-03-02 18:53:32 +0900

    add: biome for formatter & linter & apply

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
-  return (
-    <>
-      <title>HonoX</title>
-      <h1>Welcome to the HonoX website</h1>
-    </>
-  )
+	return (
+		<>
+			<title>HonoX</title>
+			<h1>Welcome to the HonoX website</h1>
+		</>
+	);
 }
