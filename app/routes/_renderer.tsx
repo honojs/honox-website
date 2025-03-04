4a4992b542d5b787b4fa7dd11bb2ea841898c3dd
Yusuke Wada <yusuke@kamawada.com>
2025-03-04 16:46:34 +0900

    chore: setup eslint and prettier (#5)
    * chore: setup eslint and prettier
    
    * add `.prettierrc`

--- app/routes/_renderer.tsx
+++ app/routes/_renderer.tsx
@@ -2,10 +2,10 @@ import { jsxRenderer } from 'hono/jsx-renderer'
 
 export default jsxRenderer(({ children }) => {
   return (
-    <html lang="en">
+    <html lang='en'>
       <head>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta charset='UTF-8' />
+        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
       </head>
       <body>{children}</body>
     </html>
